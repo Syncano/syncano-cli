7eabc9dbf2771ee3907d382450f07144a963156e
Daniel Kopka <daniel.kopka@gmail.com>
2015-02-06 15:29:06 +0100

    draft

--- syncanocli/commands/auth.py
+++ syncanocli/commands/auth.py
@@ -1,6 +1,26 @@
 import click
 
 
-@click.command('auth')
+@click.group('auth')
 def cli(*args, **kwargs):
-    pass
+    '''Authenticate, display api key and current user'''
+
+
+@cli.command()
+def login():
+    '''Log in with your Syncano credentials'''
+
+
+@cli.command()
+def logout():
+    '''Clear local authentication credentials'''
+
+
+@cli.command()
+def api_key():
+    '''Display your current api key'''
+
+
+@cli.command()
+def whoami():
+    '''Display your Syncano email address'''
