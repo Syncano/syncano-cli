97e9a95e8d1b6c44250f872675763a411041a039
andjelx <andjel@gmail.com>
2016-05-31 21:41:58 +0300

    Sub diff_dicts been rewriten

--- syncano_cli/sync/utils.py
+++ syncano_cli/sync/utils.py
@@ -6,13 +6,10 @@ def compare_dicts(new, old):
     """
     Compare two dicts returning same, added, removed, modified elements as lists
     """
-    new_keys = set(new.keys())
-    if not old:
-        return None, set(new.keys()), None, None
-    old_keys = set(old.keys())
-    intersect_keys = new_keys.intersection(old_keys)
+    old_keys = set(old or ())
+    new_keys = set(new or ())
+    common = new_keys & old_keys
     added = new_keys - old_keys
     removed = old_keys - new_keys
-    modified = {o: (new[o], old[o]) for o in intersect_keys if new[o] != old[o]}
-    same = set(o for o in intersect_keys if new[o] == old[o])
-    return same, added, removed, modified
+    same = set(k for k in common if old[k] == new[k])
+    return same, added, removed, common - same
