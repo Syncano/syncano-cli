d40ba3bcceb1dcc7338b689194cd7214d7b2d5ff
Marcin Skiba <marcinskiba91@gmail.com>
2016-06-08 13:20:20 +0200

    Refactor the code: add error handling of invalid or empty parameters, improve output format.

--- syncano_cli/execute/commands.py
+++ syncano_cli/execute/commands.py
@@ -6,6 +6,7 @@ import sys
 from ConfigParser import NoOptionError
 
 import click
+from syncano.exceptions import SyncanoDoesNotExist
 from syncano_cli import LOG
 
 from .connection import create_connection
@@ -27,11 +28,20 @@ def execute(config, instance_name, script_endpoint_name, payload):
     """
     try:
         connection = create_connection(config)
+        instance = connection.Instance.please.get(instance_name)
+        se = instance.script_endpoints.get(instance_name, script_endpoint_name)
+        data = json.loads((payload or '').strip() or '{}')
+        response = se.run(**data)
+        if response.status == 'success':
+            print(json.dumps(response.result['stdout'], indent=4, sort_keys=True))
+        else:
+            LOG.error(response.result['stderr'])
     except NoOptionError:
         LOG.error('Do a login first: syncano login.')
         sys.exit(1)
-    instance = connection.Instance.please.get(instance_name)
-    se = instance.script_endpoints.get(instance_name, script_endpoint_name)
-    data = json.loads(payload.strip() or '{}')
-    response = se.run(**data)
-    print(json.dumps(response.result, indent=4, sort_keys=True))
+    except SyncanoDoesNotExist as e:
+        LOG.error(e)
+        sys.exit(1)
+    except ValueError as e:
+        LOG.error('Invalid payload format: {error}'.format(error=e))
+        sys.exit(1)
