b8d319db390b6206386f881d2ca27fa07665f8ea
Sebastian Opałczyński <opalczynski@gmail.com>
2016-07-26 16:26:40 +0200

    [CORE-1601] simply the tests;

--- tests/test_hosting_commands.py
+++ tests/test_hosting_commands.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from syncano_cli.hosting.utils import HostingCommands
 from syncano_cli.main import cli
 from tests.base import BaseCLITest
 
@@ -7,7 +6,6 @@ from tests.base import BaseCLITest
 class HostingCommandsTest(BaseCLITest):
 
     def test_file_list(self):
-        self._create_hosting()
         self._publish_files()
 
         result = self.runner.invoke(cli, args=[
@@ -16,13 +14,6 @@ class HostingCommandsTest(BaseCLITest):
 
         self.assertIn('index.html', result.output)
         self.assertIn('css/page.css', result.output)
-
-    def test_publish_files(self):
-        self._publish_files()
-
-    def _create_hosting(self):
-        hosting_commands = HostingCommands(instance=self.instance)
-        hosting_commands.create_hosting(label='Default domain', domain=['default'])
 
     def _publish_files(self):
         result = self.runner.invoke(cli, args=[
